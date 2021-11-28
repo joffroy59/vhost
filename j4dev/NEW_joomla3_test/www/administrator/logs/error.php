8f974376bca2cab0efaacb6c9c74c2e3ea51ecef
Jerome OFFORY <jerome.offroy@gmail.com>
2021-11-28 14:30:40 +0100

    chore(NEW_joomla3_test): copy from NEW_joomla3_default

---
+++ j4dev/NEW_joomla3_test/www/administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-11-28 01:04:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-11-28T01:04:18+00:00	INFO 172.21.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-11-28T01:05:15+00:00	INFO 172.21.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
