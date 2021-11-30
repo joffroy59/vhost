020c676f07092cfec6a6078e7403b65f5b1c46bf
root <root@pcASGLM.localdomain>
2021-11-30 22:26:12 +0100

    update joomla 3.10.3

--- j4dev/NEW_joomla3_helixultimate_stater/www/administrator/logs/joomla_update.php
+++ j4dev/NEW_joomla3_helixultimate_stater/www/administrator/logs/joomla_update.php
@@ -31,3 +31,11 @@
 2021-09-16T07:04:57+00:00	INFO ::1	update	Deleting removed files and folders.
 2021-09-16T07:04:58+00:00	INFO ::1	update	Cleaning up after installation.
 2021-09-16T07:04:58+00:00	INFO ::1	update	Update to version 3.10.2 is complete.
+2021-11-30T21:20:41+00:00	INFO 172.23.0.1	update	Update started by user Super User (403). Old version is 3.10.2.
+2021-11-30T21:20:42+00:00	INFO 172.23.0.1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-10-3/Joomla_3.10.3-Stable-Update_Package.zip.
+2021-11-30T21:20:46+00:00	INFO 172.23.0.1	update	File Joomla_3.10.3-Stable-Update_Package.zip downloaded.
+2021-11-30T21:20:46+00:00	INFO 172.23.0.1	update	Starting installation of new version.
+2021-11-30T21:25:01+00:00	INFO 172.23.0.1	update	Finalising installation.
+2021-11-30T21:25:01+00:00	INFO 172.23.0.1	update	Deleting removed files and folders.
+2021-11-30T21:25:04+00:00	INFO 172.23.0.1	update	Cleaning up after installation.
+2021-11-30T21:25:04+00:00	INFO 172.23.0.1	update	Update to version 3.10.3 is complete.
