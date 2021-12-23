de85ae3909fa139f038a06e161296c99e6ffd2e6
Jerome OFFORY <jerome.offroy@gmail.com>
2021-12-23 21:56:39 +0100

    chore(NEW_joomla3_helixultimate_stater): a lot of thing

---
+++ j4dev/NEW_joomla3_helixultimate_stater/www/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-12-15 20:48:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-12-15T20:48:21+00:00	INFO 172.21.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
