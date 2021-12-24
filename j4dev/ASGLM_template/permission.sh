56345dba8b7eff5b55684528a6087058398cb1a5
Jerome OFFORY <jerome.offroy@gmail.com>
2021-12-24 15:22:33 +0100

    fix

--- j4dev/ASGLM_template/permission.sh
+++ j4dev/ASGLM_template/permission.sh
@@ -1,10 +1,10 @@
-cat <<- EOD
+#!/bin/bash
+
 WLUSER=joffroy
 PROD_FILE="LICENSE.txt htaccess.txt web.config.txt README.txt configuration.php index.php robots.txt.dist"
 PROD_DIR="administrator cache components includes installation layouts media plugins templates api cli images language libraries modules tmp"
 cd www
-find \$(echo \$PROD_DIR) -type -d -exec sudo chmod g+s {} \;
-sudo chmod -R g+w  \$(echo \$PROD_DIR)
-sudo chown -R www-data:${WLUSER} \$(echo \$PROD_DIR)
-sudo chown -R www-data:${WLUSER} \$(echo \$PROD_FILE)
-EOD
+find "$PROD_DIR" -type -d -exec sudo chmod g+s {} \;
+sudo chmod -R g+w $PROD_DIR
+sudo chown -R www-data:${WLUSER} $PROD_DIR
+sudo chown -R www-data:${WLUSER} $PROD_FILE
