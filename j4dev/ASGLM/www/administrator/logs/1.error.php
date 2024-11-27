f94639a8c929a6fed6f9f9da646d5cbeb22e9544
Jerome OFFORY <jerome.offroy@gmail.com>
2024-11-27 20:54:18 +0100

    tmp(): ???

--- j4dev/ASGLM/www/administrator/logs/1.error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2021-12-05 20:10:33 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2021-12-05T20:10:33+00:00	INFO 172.18.0.1	joomlafailure	Le nom d'utilisateur ne correspond pas au mot de passe, ou vous n'avez pas encore de compte.
